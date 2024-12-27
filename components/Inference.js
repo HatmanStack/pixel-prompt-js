2e85c9454bae32e5d1caf33422b26ddc89eaa0a7
hatmanstack <hatmanstack@protonmail.com>
2024-12-27 09:23:21 -0600

    Error check for s3 fetch

--- components/Inference.js
+++ components/Inference.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
 function getScaledIP(styleSwitch, settingSwitch) {
-  let scaledIP = 'Load original IP-Adapter';
+  let scaledIP = "Load original IP-Adapter";
   if (styleSwitch) {
     scaledIP = "Load only style blocks";
   }
@@ -16,63 +16,68 @@ function getScaledIP(styleSwitch, settingSwitch) {
 }
 
 const Inference = ({
-      setImageSource,
-      setPromptList,
-      selectedImageIndex,
-      setInferrenceButton,
-      inferrenceButton,
-      setModelMessage,
-      imageSource,
-      modelID,
-      prompt,
-      styleSwitch,
-      settingSwitch,
-      control,
-      guidance,
-      steps,
-      setActivity,
-      setModelError,
-      setReturnedPrompt,
-      setInitialReturnedPrompt,
-      setInferredImage,
+  setImageSource,
+  setPromptList,
+  selectedImageIndex,
+  setInferrenceButton,
+  inferrenceButton,
+  setModelMessage,
+  imageSource,
+  modelID,
+  prompt,
+  styleSwitch,
+  settingSwitch,
+  control,
+  guidance,
+  steps,
+  setActivity,
+  setModelError,
+  setReturnedPrompt,
+  setInitialReturnedPrompt,
+  setInferredImage,
 }) => {
- 
   useEffect(() => {
     const fetchImagesFromS3 = async () => {
       try {
-        const AWS = require('aws-sdk');
-        
+        const AWS = require("aws-sdk");
 
         const s3 = new AWS.S3({
           region: Constants.manifest.extra.AWS_REGION,
           accessKeyId: Constants.manifest.extra.AWS_ID,
-          secretAccessKey: Constants.manifest.extra.AWS_SECRET
+          secretAccessKey: Constants.manifest.extra.AWS_SECRET,
         });
-        
-        const params = {
-          Bucket: Constants.manifest.extra.S3_BUCKET
-        };
-        
-        const data = await s3.listObjectsV2(params).promise();
-        
-        for (const item of data.Contents) {
-          
-          const objectParams = {
-            Bucket: Constants.manifest.extra.S3_BUCKET,
-            Key: item.Key
-          };
-          
-          const objectData = await s3.getObject(objectParams).promise();
-          const jsonData = JSON.parse(objectData.Body.toString('utf-8'));
-          
-          const base64Data = jsonData.base64image; 
-          const prompt = jsonData.returnedPrompt; 
-          setPromptList(prevPromptList => [prompt, ...prevPromptList]);
-          setImageSource(prevImageSource => [base64Data, ...prevImageSource]);
-        }
 
+        const params = {
+          Bucket: Constants.manifest.extra.S3_BUCKET,
+        };
+
+        const data = await s3.listObjectsV2(params).promise();
+
+        for (const item of data.Contents) {
+          if (item.Key === "images/") continue;
+          try {
+            const objectParams = {
+              Bucket: Constants.manifest.extra.S3_BUCKET,
+              Key: item.Key,
+            };
+
+            const objectData = await s3.getObject(objectParams).promise();
+            const jsonData = JSON.parse(objectData.Body.toString("utf-8"));
+
+            const base64Data = jsonData.base64image;
+            const prompt = jsonData.returnedPrompt;
+            setPromptList((prevPromptList) => [prompt, ...prevPromptList]);
+            setImageSource((prevImageSource) => [
+              base64Data,
+              ...prevImageSource,
+            ]);
+          } catch (error) {
+            console.error(`Error processing ${item.Key}:`, error);
+            continue;
+          }
+        }
       } catch (error) {
-        console.error('Error fetching images from S3:', error);
+        console.error("Error fetching images from S3:", error);
       }
     };
 
@@ -86,16 +91,16 @@ const Inference = ({
       const ipScaleHolder = getScaledIP(styleSwitch, settingSwitch);
       const controlImage = imageSource[selectedImageIndex];
 
-      const AWS = require('aws-sdk');
+      const AWS = require("aws-sdk");
       const lambda = new AWS.Lambda({
         region: Constants.manifest.extra.AWS_REGION,
         accessKeyId: Constants.manifest.extra.AWS_ID,
-        secretAccessKey: Constants.manifest.extra.AWS_SECRET
+        secretAccessKey: Constants.manifest.extra.AWS_SECRET,
       });
 
       const params = {
         FunctionName: Constants.manifest.extra.AWS_LAMBDA_FUNCTION,
-        InvocationType: 'RequestResponse',
+        InvocationType: "RequestResponse",
         Payload: JSON.stringify({
           prompt: prompt,
           steps: steps,
@@ -104,42 +109,51 @@ const Inference = ({
           image: controlImage,
           target: ipScaleHolder,
           control: control,
-          task: "image"
-        })
+          task: "image",
+        }),
       };
 
-      lambda.invoke(params)
+      lambda
+        .invoke(params)
         .promise()
-        .then(data => {
+        .then((data) => {
           const jsonHolder = JSON.parse(data.Payload).body;
           const responseData = JSON.parse(jsonHolder);
           console.log(responseData);
           if (/Model Waking/.test(responseData.output)) {
             setModelMessage("Model Waking");
             setModelError(true);
-          } else if(/You have exceeded your GPU quota/.test(responseData.output)) {
+          } else if (
+            /You have exceeded your GPU quota/.test(responseData.output)
+          ) {
             const gpu = responseData.output.split(": ")[2];
             const gpuName = gpu.slice(-9);
-            setModelMessage(`GPU Quota Exceeded! Try Random Models without enlarged images for ${gpuName.slice(0,-1)}`);
+            setModelMessage(
+              `GPU Quota Exceeded! Try Random Models without enlarged images for ${gpuName.slice(0, -1)}`
+            );
             setModelError(true);
-          } else if(/An error occurred/.test(responseData.output)) {
+          } else if (/An error occurred/.test(responseData.output)) {
             setModelMessage(`Model Error!`);
             setModelError(true);
           } else {
-            setInitialReturnedPrompt("Model:\n" + responseData.model + "\n\nPrompt:\n" + prompt);
+            setInitialReturnedPrompt(
+              "Model:\n" + responseData.model + "\n\nPrompt:\n" + prompt
+            );
             setModelError(false);
           }
-          if(responseData.NSFW) {
+          if (responseData.NSFW) {
             setModelMessage(`NSFW...Image will not be Saved`);
             setModelError(true);
           }
 
           setInferrenceButton(false);
           setActivity(false);
-          setReturnedPrompt("Model:\n" + responseData.model + "\n\nPrompt:\n" + prompt);
+          setReturnedPrompt(
+            "Model:\n" + responseData.model + "\n\nPrompt:\n" + prompt
+          );
           setInferredImage("data:image/png;base64," + responseData.output);
         })
-        .catch(error => {
+        .catch((error) => {
           setModelMessage("Application Error!");
           setActivity(false);
           setModelError(true);
